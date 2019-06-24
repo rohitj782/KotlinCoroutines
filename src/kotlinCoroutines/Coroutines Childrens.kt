03378cbe3e5f99e2e50b29bb9e247c117a5e0662
Rohit Joshi <44168655+rohitj782@users.noreply.github.com>
2019-06-24 06:22:44 +0530

    Coroutine Childrens

--- src/kotlinCoroutines/Coroutines Childrens.kt
+++ src/kotlinCoroutines/Coroutines Childrens.kt
@@ -1,2 +1,30 @@
-package kotlinCoroutines
-
+//package kotlinCoroutines
+//
+//import kotlinx.coroutines.GlobalScope
+//import kotlinx.coroutines.delay
+//import kotlinx.coroutines.launch
+//import kotlinx.coroutines.runBlocking
+//
+//fun main() = runBlocking {
+//    // launch a coroutine to process some kind of incoming request
+//    val request = launch {
+//        // it spawns two other jobs, one with GlobalScope
+//        GlobalScope.launch {
+//            println("job1: I run in GlobalScope and execute independently!")
+//            delay(1000)
+//            println("job1: I am not affected by cancellation of the request")
+//        }
+//        // and the other inherits the parent context
+//        launch {
+//            delay(100)
+//            println("job2: I am a child of the request coroutine")
+//            delay(1000)
+//            println("job2: I will not execute this line if my parent request is cancelled")
+//        }
+//    }
+//    delay(500)
+//    request.cancel() // cancel processing of the request
+//    delay(1000) // delay a second to see what happens
+//    println("main: Who has survived request cancellation?")
+//
+//}
