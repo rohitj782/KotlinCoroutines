1007d983a4e1c83d33421f104c53226b9f193d13
Rohit Joshi <44168655+rohitj782@users.noreply.github.com>
2019-06-24 06:16:35 +0530

    Dispatcheres And Debugging

---
+++ src/kotlinCoroutines/Debugging Coroutines.kt
@@ -0,0 +1,20 @@
+//package kotlinCoroutines
+//
+//import kotlinx.coroutines.async
+//import kotlinx.coroutines.runBlocking
+//
+//fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+//
+//fun main() = runBlocking<Unit> {
+//    //sampleStart
+//    val a = async {
+//        log("I'm computing a piece of the answer")
+//        6
+//    }
+//    val b = async {
+//        log("I'm computing another piece of the answer")
+//        7
+//    }
+//    log("The answer is ${a.await() * b.await()}")
+////sampleEnd
+//}
