364e226919b7745205a77000018b74f4cc98d24f
Rohit Joshi <44168655+rohitj782@users.noreply.github.com>
2019-07-21 07:39:54 +0530

    Debugging Cororutines

--- src/kotlinCoroutines/Debugging Coroutines.kt
+++ src/kotlinCoroutines/Debugging Coroutines.kt
@@ -1,20 +1,40 @@
-//package kotlinCoroutines
+////package kotlinCoroutines
+////
+////import kotlinx.coroutines.async
+////import kotlinx.coroutines.runBlocking
+////
+////fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+////
+////fun main() = runBlocking<Unit> {
+////    //sampleStart
+////    val a = async {
+////        log("I'm computing a piece of the answer")
+////        6
+////    }
+////    val b = async {
+////        log("I'm computing another piece of the answer")
+////        7
+////    }
+////    log("The answer is ${a.await() * b.await()}")
+//////sampleEnd
+////}
 //
-//import kotlinx.coroutines.async
-//import kotlinx.coroutines.runBlocking
+//import kotlinx.coroutines.*
 //
 //fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 //
-//fun main() = runBlocking<Unit> {
-//    //sampleStart
-//    val a = async {
-//        log("I'm computing a piece of the answer")
+//fun main() = runBlocking(CoroutineName("main")) {
+//    log("Started main coroutine")
+//    // run two background value computations
+//    val v1 = async(CoroutineName("v1coroutine")) {
+//        delay(500)
+//        log("Computing v1")
+//        252
+//    }
+//    val v2 = async(CoroutineName("v2coroutine")) {
+//        delay(1000)
+//        log("Computing v2")
 //        6
 //    }
-//    val b = async {
-//        log("I'm computing another piece of the answer")
-//        7
-//    }
-//    log("The answer is ${a.await() * b.await()}")
-////sampleEnd
+//    log("The answer for v1 / v2 = ${v1.await() / v2.await()}")
 //}
